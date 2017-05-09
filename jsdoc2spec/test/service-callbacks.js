05aa1159acc6ead662ab8bfd1e070bbf024b226f
yoav@wix.com <yoav@wix.com>
2017-05-09 17:20:00 +0300

    added a complex callback example

--- jsdoc2spec/test/service-callbacks.js
+++ jsdoc2spec/test/service-callbacks.js
@@ -26,6 +26,36 @@ class ServiceCallbacks {
     operationWithCallback(input, callback) {
 
     }
+
+    /**
+     * @callback aComplexCallbackCallback
+     * @memberof aNamespace.ServiceCallbacks
+     * @param {number} z -
+     */
+
+    /**
+     * @callback aComplexCallback
+     * @memberof aNamespace.ServiceCallbacks
+     * @param {number} x -
+     * @param {string} y -
+     * @param {aNamespace.ServiceCallbacks.aComplexCallbackCallback} cb -
+     * @returns {Promise<string>}
+     */
+
+    /**
+     * @function operationWithComplexCallback
+     * @memberof aNamespace.ServiceCallbacks
+     * @summary an operation
+     * @description
+     * the description of the operation
+     *
+     * @param {string} input the input
+     * @param {aNamespace.ServiceCallbacks.aComplexCallback} callback the input
+     */
+    operationWithComplexCallback(input, callback) {
+
+    }
+
 }
 
 
--- jsdoc2spec/test/service-callbacks.spec.js
+++ jsdoc2spec/test/service-callbacks.spec.js
@@ -24,7 +24,7 @@ describe('docs', function() {
         });
 
 
-        it.only('should support function callbacks', function() {
+        it('should support function callbacks', function() {
             expect(jsDocRes).to.containSubset({
                 services: [
                     {
@@ -38,6 +38,32 @@ describe('docs', function() {
                         callbacks: [
                             {name: 'aCallback', nameParams: [], params: [
                                 {name: 'x', type: 'number'},
+                            ], ret: 'void'}
+                        ]
+                    }
+                ]
+            });
+        });
+
+        it.only('should support function with complex callbacks', function() {
+            expect(jsDocRes).to.containSubset({
+                services: [
+                    {
+                        name: 'ServiceCallbacks',
+                        operations: [
+                            {name: 'operationWithComplexCallback', nameParams: [], params: [
+                                {name: 'input', type: 'string'},
+                                {name: 'callback', type: 'aNamespace.ServiceCallbacks.aComplexCallback'}
+                            ], ret: 'void'}
+                        ],
+                        callbacks: [
+                            {name: 'aComplexCallback', nameParams: [], params: [
+                                {name: 'x', type: 'number'},
+                                {name: 'y', type: 'string'},
+                                {name: 'cb', type: 'aNamespace.ServiceCallbacks.aComplexCallbackCallback'}
+                            ], ret: 'Promise.<string>'},
+                            {name: 'aComplexCallbackCallback', nameParams: [], params: [
+                                {name: 'z', type: 'number'}
                             ], ret: 'void'}
                         ]
                     }
