50392c5f7973311fb703162043e4fde80bfd989a
yoav@wix.com <yoav@wix.com>
2017-05-09 10:34:57 +0300

    testing different (simple) data types

---
+++ jsdoc2spec/test/service-types.js
@@ -0,0 +1,68 @@
+
+
+/**
+ * @class ServiceTypes
+ * @summary A test service
+ * @memberof aNamespace
+ */
+class ServiceTypes {
+    /**
+     * @member aString
+     * @memberof aNamespace.ServiceTypes
+     * @type {string}
+     */
+    get aString() {
+    }
+
+    /**
+     * @member aNumber
+     * @memberof aNamespace.ServiceTypes
+     * @type {number}
+     */
+    get aNumber() {
+    }
+
+    /**
+     * @member aBoolean
+     * @memberof aNamespace.ServiceTypes
+     * @type {boolean}
+     */
+    get aBoolean() {
+    }
+
+    /**
+     * @member aDate
+     * @memberof aNamespace.ServiceTypes
+     * @type {Date}
+     */
+    get aDate() {
+    }
+
+    /**
+     * @member union
+     * @memberof aNamespace.ServiceTypes
+     * @type {string | number}
+     */
+    get union() {
+    }
+
+    /**
+     * @member anArray
+     * @memberof aNamespace.ServiceTypes
+     * @type {string[]}
+     */
+    get anArray() {
+    }
+
+    /**
+     * @member anArray2
+     * @memberof aNamespace.ServiceTypes
+     * @type {Array.<string>}
+     */
+    get anArray2() {
+    }
+
+}
+
+
+export default ServiceTypes;
---
+++ jsdoc2spec/test/service-types.spec.js
@@ -0,0 +1,119 @@
+import runJsDoc from '../lib/jsdoc-runner';
+import {dump} from '../lib/util';
+import chai from 'chai';
+import chaiSubset from 'chai-subset';
+const expect = chai.expect;
+chai.use(chaiSubset);
+
+describe('docs', function() {
+    describe.only('service types', function() {
+        let jsDocRes;
+        beforeEach(() => {
+            jsDocRes = runJsDoc({
+                "include": [
+                    "test/service-types.js"
+                ]
+            });
+        });
+
+        afterEach(function(){
+            if (this.currentTest.state == 'failed') {
+                console.log('the jsDocRes:');
+                dump(jsDocRes);
+            }
+        });
+
+
+        it('should support string type', function() {
+            expect(jsDocRes).to.containSubset({
+                services: [
+                    {
+                        name: 'ServiceTypes',
+                        properties: [
+                            {name: 'aString', get: true, set: false, type: 'string'}
+                        ]
+                    }
+                ]
+            });
+        });
+
+        it('should support number type', function() {
+            expect(jsDocRes).to.containSubset({
+                services: [
+                    {
+                        name: 'ServiceTypes',
+                        properties: [
+                            {name: 'aNumber', get: true, set: false, type: 'number'}
+                        ]
+                    }
+                ]
+            });
+        });
+
+        it('should support boolean type', function() {
+            expect(jsDocRes).to.containSubset({
+                services: [
+                    {
+                        name: 'ServiceTypes',
+                        properties: [
+                            {name: 'aBoolean', get: true, set: false, type: 'boolean'}
+                        ]
+                    }
+                ]
+            });
+        });
+
+        it('should support date type', function() {
+            expect(jsDocRes).to.containSubset({
+                services: [
+                    {
+                        name: 'ServiceTypes',
+                        properties: [
+                            {name: 'aDate', get: true, set: false, type: 'Date'}
+                        ]
+                    }
+                ]
+            });
+        });
+
+        it('should support union type defined as string | number', function() {
+            expect(jsDocRes).to.containSubset({
+                services: [
+                    {
+                        name: 'ServiceTypes',
+                        properties: [
+                            {name: 'union', get: true, set: false, type: ['string', 'number']}
+                        ]
+                    }
+                ]
+            });
+        });
+
+        it('should support array type defined as string[]', function() {
+            expect(jsDocRes).to.containSubset({
+                services: [
+                    {
+                        name: 'ServiceTypes',
+                        properties: [
+                            {name: 'anArray', get: true, set: false, type: 'Array.<string>'}
+                        ]
+                    }
+                ]
+            });
+        });
+
+        it('should support array type defined as Array.<string>', function() {
+            expect(jsDocRes).to.containSubset({
+                services: [
+                    {
+                        name: 'ServiceTypes',
+                        properties: [
+                            {name: 'anArray2', get: true, set: false, type: 'Array.<string>'}
+                        ]
+                    }
+                ]
+            });
+        });
+
+    });
+});
