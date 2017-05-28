0c3fc18350ee102d5ca937e603b885c08c1cea3d
yoav@wix.com <yoav@wix.com>
2017-05-28 16:56:39 +0300

    added support for nested objects

--- docworks-json/index.js
+++ docworks-json/index.js
@@ -1,27 +1,55 @@
+import isArray from 'lodash.isarray';
+import isObject from 'lodash.isobject';
+
 export function toJson(obj, indentSize, spec) {
     return serialize(obj, 0, indentSize, spec);
 }
 
 function serialize(obj, indentPos, indentSize, spec) {
-    let indent = new Array((indentPos+1)*indentSize + 1).join(' ');
+    let indent = new Array((indentPos)*indentSize + 1).join(' ');
+    let indentChilds = new Array((indentPos+1)*indentSize + 1).join(' ');
     let names = Object.getOwnPropertyNames(obj);
-    let orderedNames = (spec || {}).order || [];
+    let orderedNames = specToOrderedNames(spec);
     let props = [];
+
+    function handleValue(name, value, valueSpec) {
+        if (isArray(value))
+            ;
+        else if (isObject(value))
+            props.push(`${indentChilds}${name}:` + serialize(value, indentPos+1, indentSize, valueSpec));
+        else
+            props.push(`${indentChilds}${name}:` + JSON.stringify(value));
+    }
 
     for (let i in orderedNames) {
         let name = orderedNames[i];
         if (obj[name]) {
-            props.push(`${indent}${name}:` + JSON.stringify(obj[name]));
+            handleValue(name, obj[name], spec[name]);
         }
     }
 
     for (let i in names) {
         let name = names[i];
         if (!orderedNames.find(_ => _ === name)) {
-            props.push(`${indent}${name}:` + JSON.stringify(obj[name]));
+            handleValue(name, obj[name]);
         }
     }
     return '{\n' +
         props.join(',\n') +
-        '\n}';
+        `\n${indent}}`;
+}
+
+function specToOrderedNames(spec){
+    spec = spec || [];
+    let keys = Object.keys(spec);
+    return keys.map(key => {
+        let value = spec[key];
+        if (value && value.pos)
+            return [key, value.pos];
+        else
+            return undefined;
+    })
+        .filter(_ => !!_)
+        .sort((a,b) => a[0] - b[0])
+        .map(_ => _[0])
 }
--- docworks-json/test/json-format.spec.js
+++ docworks-json/test/json-format.spec.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 const expect = chai.expect;
 import {toJson, fromJson} from '../index';
+import trimEnd from 'lodash.trimend';
 
 
 describe('toJson', function() {
@@ -25,7 +26,7 @@ describe('toJson', function() {
             x: 4,
             r: 3
         };
-        let json = toJson(obj, 2, {order: ['a', 'b', 'r', 'x', 'z']});
+        let json = toJson(obj, 2, {a: {pos:1}, b: {pos:2}, r:{pos:3}, x:{pos:4}, z:{pos:5}});
         expect(json).to.equal(stripMargin(`{
           |  a:1,
           |  b:2,
@@ -44,7 +45,7 @@ describe('toJson', function() {
             x: 4,
             r: 3
         };
-        let json = toJson(obj, 2, {order: ['z', 'a', 'r', 'b', 'x']});
+        let json = toJson(obj, 2, {z: {pos:1}, a:{pos:2}, r:{pos:3}, b:{pos:4}, x:{pos:5}});
         expect(json).to.equal(stripMargin(`{
           |  z:5,
           |  a:1,
@@ -54,6 +55,57 @@ describe('toJson', function() {
           |}`
         ))
     });
+
+    it('should order print all members, even those not in the order array', function() {
+        let obj = {
+            a: 1,
+            z: 5,
+            b: 2,
+            x: 4,
+            r: 3
+        };
+        let json = toJson(obj, 2, {z:{pos:1}, a:{pos:2}, r:{pos:3}});
+        expect(json).to.equal(stripMargin(`{
+          |  z:5,
+          |  a:1,
+          |  r:3,
+          |  b:2,
+          |  x:4
+          |}`
+        ))
+    });
+
+    it('should support nested objects', function() {
+        let obj = {
+            a: 1,
+            z: 5,
+            b: 2,
+            x: {
+                aa: 1,
+                zz: 3,
+                cc: 2
+            }
+        };
+        let json = toJson(obj, 2, {
+            z: {pos: 1},
+            a: {pos: 2},
+            x: {pos: 3, aa: {pos: 1}, cc: {pos: 2}, zz: {pos: 3}},
+            r: {pos: 4}
+        });
+
+        expect(json).to.equal(stripMargin(`{
+          |  z:5,
+          |  a:1,
+          |  x:{
+          |    aa:1,
+          |    cc:2,
+          |    zz:3
+          |  },
+          |  b:2
+          |}`
+        ))
+    });
+
 });
 
 function stripMargin(string) {
@@ -62,5 +114,6 @@ function stripMargin(string) {
             let parts = line.split('|');
             return (parts.length == 2)?parts[1]: parts[0]
         })
+        .map(trimEnd)
         .join('\n');
 }
