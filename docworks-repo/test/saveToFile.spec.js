7054ab91f1f7dda1501084c3b9793778799939f9
yoav@wix.com <yoav@wix.com>
2017-07-03 11:36:47 +0300

    fixed race condition in the test

--- docworks-repo/test/saveToFile.spec.js
+++ docworks-repo/test/saveToFile.spec.js
@@ -2,6 +2,7 @@ import chai from 'chai';
 import chaiFiles from 'chai-files';
 import runJsDoc from 'docworks-jsdoc2spec';
 import {saveToDir} from '../index';
+import fs from 'fs-extra';
 
 chai.use(chaiFiles);
 const expect = chai.expect;
@@ -10,6 +11,10 @@ const dir = chaiFiles.dir;
 
 
 describe('saveToFiles', function() {
+    beforeEach(() => {
+        return fs.remove('tmp');
+    });
+
     it('should save all services to files', function() {
         let jsDocRes = runJsDoc({
             "include": [
@@ -17,18 +22,20 @@ describe('saveToFiles', function() {
             ],
             "includePattern": ".+\\.(js)?$",
         });
-        saveToDir('tmp', jsDocRes.services);
-        expect(dir('tmp/aNamespace')).to.exist;
-        expect(file('tmp/aNamespace.service.json')).to.exist;
-        expect(file('tmp/aNamespace/aMixes.service.json')).to.exist;
-        expect(file('tmp/aNamespace/child.service.json')).to.exist;
-        expect(file('tmp/aNamespace/Service.service.json')).to.exist;
-        expect(file('tmp/aNamespace/ServiceCallbacks.service.json')).to.exist;
-        expect(file('tmp/aNamespace/ServiceDocs.service.json')).to.exist;
-        expect(file('tmp/aNamespace/ServiceMessages.service.json')).to.exist;
-        expect(file('tmp/aNamespace/ServiceOperations.service.json')).to.exist;
-        expect(file('tmp/aNamespace/ServiceProperties.service.json')).to.exist;
-        expect(file('tmp/aNamespace/ServiceTypes.service.json')).to.exist;
+        return saveToDir('tmp', jsDocRes.services)
+          .then(() => {
+              expect(dir('tmp/aNamespace')).to.exist;
+              expect(file('tmp/aNamespace.service.json')).to.exist;
+              expect(file('tmp/aNamespace/aMixes.service.json')).to.exist;
+              expect(file('tmp/aNamespace/child.service.json')).to.exist;
+              expect(file('tmp/aNamespace/Service.service.json')).to.exist;
+              expect(file('tmp/aNamespace/ServiceCallbacks.service.json')).to.exist;
+              expect(file('tmp/aNamespace/ServiceDocs.service.json')).to.exist;
+              expect(file('tmp/aNamespace/ServiceMessages.service.json')).to.exist;
+              expect(file('tmp/aNamespace/ServiceOperations.service.json')).to.exist;
+              expect(file('tmp/aNamespace/ServiceProperties.service.json')).to.exist;
+              expect(file('tmp/aNamespace/ServiceTypes.service.json')).to.exist;
+          });
     });
 
 });
