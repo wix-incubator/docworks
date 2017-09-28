fd1b91052c458e93a4163f3cea375bfce275aed5
yoav@wix.com <yoav@wix.com>
2017-09-28 15:34:49 +0300

    index.js refactoring

--- docworks-repo/index.js
+++ docworks-repo/index.js
@@ -1,33 +1,11 @@
-import {serviceToFileName, serviceToDirName, serviceToJson, readDir, zipByName} from './lib/operations';
+import {readDir} from './lib/operations';
+export {saveToDir} from './lib/operations';
 export merge from './lib/merge';
-import fs from 'fs-extra';
-
-export async function saveToDir(directory, services) {
-  let filesAndServices = services.map(service => {
-    let dirName = serviceToDirName(directory, service);
-    let fileName = serviceToFileName(directory, service);
-    let serviceJson = serviceToJson(service);
-    return [dirName, fileName, serviceJson];
-  });
-
-  // ensure all directories are created before starting to save files - we do so one after the other
-  let dirNames = new Set(filesAndServices.map(_ => _[0]));
-  let dirsPromise = [...dirNames].reduce(function(cur, next) {
-    return cur.then(() => fs.ensureDir(next))
-  }, Promise.resolve());
-
-  // now we can save files in parallel
-  return dirsPromise.then(() => {
-    return Promise.all(
-      filesAndServices.map(
-        (fileAndService) => fs.outputFile(fileAndService[1], fileAndService[2])))
-  });
-}
 
 export function readFromDir(directory) {
   return readDir(directory)
-    .then((services) => {return {
-      services
-    }});
+    .then((services) => {
+      return {services}
+    });
 }
 
--- docworks-repo/lib/operations.js
+++ docworks-repo/lib/operations.js
@@ -86,6 +86,28 @@ export function serviceToJson(service) {
   return toJson(service, 2, serviceSpec)
 }
 
+export async function saveToDir(directory, services) {
+  let filesAndServices = services.map(service => {
+    let dirName = serviceToDirName(directory, service);
+    let fileName = serviceToFileName(directory, service);
+    let serviceJson = serviceToJson(service);
+    return [dirName, fileName, serviceJson];
+  });
+
+  // ensure all directories are created before starting to save files - we do so one after the other
+  let dirNames = new Set(filesAndServices.map(_ => _[0]));
+  let dirsPromise = [...dirNames].reduce(function(cur, next) {
+    return cur.then(() => fs.ensureDir(next))
+  }, Promise.resolve());
+
+  // now we can save files in parallel
+  return dirsPromise.then(() => {
+    return Promise.all(
+      filesAndServices.map(
+        (fileAndService) => fs.outputFile(fileAndService[1], fileAndService[2])))
+  });
+}
+
 
 // link - how to use async await https://stackoverflow.com/questions/33527653/babel-6-regeneratorruntime-is-not-defined-with-async-await
 export async function readDir(dir) {
