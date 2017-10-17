6c29adfce315c27151621da00e16c6182e8dcebb
yoav@wix.com <yoav@wix.com>
2017-10-17 17:26:04 +0300

    cli renamed as docworks, with multiple commands

--- docworks-cli/.bin/docworks.js
+++ docworks-cli/.bin/docworks.js
@@ -1,23 +1,35 @@
 #!/usr/bin/env node
 require("babel-register");
 require("babel-polyfill");
-var tmp = require('tmp-promise');
-var extractDocs = require('../src/extract-compare-push').default;
-var argv = require('optimist')
-  .usage('Usage: $0 -r [remote repo] -s [local sources]')
-  .demand('r')
-  .alias('r', 'remote')
-  .describe('r', 'remote repository to merge docs into')
-  .demand('s')
-  .alias('s', 'sources')
-  .describe('s', 'folder containing the source files to extract docs from')
-  .argv;
 
-let remote = argv.remote;
-let sources = argv.sources;
+if (process.argv.length < 3) {
+  printUsage();
+  process.exit(1);
+}
 
-tmp.dir().then(o => {
-  console.log('working directory', o.path);
-  return extractDocs(remote, o.path, {"include": sources, "includePattern": ".+\\.(js)?$"});
-});
+var command = process.argv[2];
+if (command === 'ecp') {
+  console.log(process.argv.slice(3));
+  var tmp = require('tmp-promise');
+  var extractDocs = require('../src/extract-compare-push').default;
+  var argv = require('optimist')
+    .usage('Usage: $0 ecp -r [remote repo] -s [local sources]')
+    .demand('r')
+    .alias('r', 'remote')
+    .describe('r', 'remote repository to merge docs into')
+    .demand('s')
+    .alias('s', 'sources')
+    .describe('s', 'folder containing the source files to extract docs from')
+    .parse(process.argv.slice(3));
+
+  let remote = argv.remote;
+  let sources = argv.sources;
+
+  tmp.dir().then(o => {
+    console.log('working directory', o.path);
+    return extractDocs(remote, o.path, {"include": sources, "includePattern": ".+\\.(js)?$"});
+  });
+
+
+}
 
