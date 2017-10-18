b6b05cb25f6d98686944317394567d9e67bd8089
yoav@wix.com <yoav@wix.com>
2017-10-18 13:22:29 +0300

    added logger colors

--- docworks-cli/src/extract-compare-push.js
+++ docworks-cli/src/extract-compare-push.js
@@ -2,58 +2,58 @@ import runJsDoc from 'docworks-jsdoc2spec';
 import {saveToDir, readFromDir, merge} from 'docworks-repo';
 import git from 'nodegit';
 
-export default async function extractDocs(remoteRepo, localFolder, jsDocSources) {
+export default async function extractDocs(remoteRepo, localFolder, jsDocSources, logger) {
   try {
 
-    console.log(`git clone ${remoteRepo} ${localFolder}`);
+    logger(`git clone ${remoteRepo} ${localFolder}`);
     await git.Clone(remoteRepo, localFolder);
 
-    console.log(`docworks readServices ${localFolder}`);
+    logger(`docworks readServices ${localFolder}`);
     let repoContent = await readFromDir(localFolder);
 
-    console.log(`docworks extractDocs ${jsDocSources.include}/**/${jsDocSources.includePattern}`);
+    logger(`docworks extractDocs ${jsDocSources.include}/**/${jsDocSources.includePattern}`);
     let newDocs = runJsDoc(jsDocSources).services;
 
-    console.log(`docworks merge`);
+    logger(`docworks merge`);
     let merged = merge(newDocs, repoContent.services);
 
-    console.log(`docworks saveServices ${localFolder}`);
+    logger(`docworks saveServices ${localFolder}`);
     await saveToDir(localFolder, merged.repo);
 
     let localRepo = await git.Repository.open(localFolder);
 
-    console.log(`git status`);
+    logger(`git status`);
     let statuses = await localRepo.getStatus();
     let files = statuses.filter(_ => _.isNew() || _.isModified())
       .map(_ => {
-        console.log(`  ${_.isNew()?'New:     ':'Modified:'} ${_.path()}`);
+        logger(`  ${_.isNew()?'New:     ':'Modified:'} ${_.path()}`);
         return _.path()
       });
 
     if (files.length > 0) {
-      console.log(`git add ${files.join(' ')}`);
+      logger(`git add ${files.join(' ')}`);
       let index = await localRepo.refreshIndex();
       await Promise.all(files.map(file => index.addByPath(file)));
       await index.write();
       let oid = await index.writeTree();
 
-      console.log(`git commit -m '${merged.messages.join('\n')}'`);
+      logger(`git commit -m '${merged.messages.join('\n')}'`);
       let head = await git.Reference.nameToId(localRepo, "HEAD");
       let parent = await localRepo.getCommit(head);
       var author = git.Signature.default(localRepo);
       var committer = git.Signature.default(localRepo);
       await localRepo.createCommit("HEAD", author, committer, merged.messages.join('\n'), oid, [parent]);
 
-      console.log('git push origin master');
+      logger('git push origin master');
       let remote = await git.Remote.lookup(localRepo, 'origin');
       await remote.push(['refs/heads/master:refs/heads/master']);
     }
     else {
-      console.log('no changes detected');
+      logger('no changes detected');
     }
-    console.log('completed workflow');
+    logger('completed workflow');
   }
   catch (error) {
-    console.log('failed to complete workflow\n', error.stack);
+    logger('failed to complete workflow\n', error.stack);
   }
 }
--- docworks-cli/test/publish-workflow.spec.js
+++ docworks-cli/test/publish-workflow.spec.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiSubset from 'chai-subset';
 import fs from 'fs-extra';
+import chalk from 'chalk';
 
 import runJsDoc from 'docworks-jsdoc2spec';
 import {saveToDir, readFromDir, merge} from 'docworks-repo';
@@ -9,46 +10,62 @@ import git from 'nodegit';
 import extractDocs from '../src/extract-compare-push';
 
 chai.use(chaiSubset);
-const expect = chai.expect;
 
-describe('compare repo', function() {
+const expect = chai.expect;
+const remote = './tmp/remote';
+const comment = (_) => console.log(`    ${chalk.gray(_)}`);
+let log = [];
+const logger = (_) => log.push(_);
+
+
+async function createRemoteOnVer1() {
+  const remoteBuild = './tmp/remoteBuild';
+  // setup
+  logger('create remote repo');
+  logger('------------------');
+  logger('git init');
+  let remoteRepo = await git.Repository.init(remoteBuild, 0);
+  // run js doc
+  logger('jsdoc ./test/ver1');
+  let v1 = runJsDoc({"include": './test/ver1', "includePattern": ".+\\.(js)?$",}).services;
+  let files = await saveToDir(remoteBuild, v1);
+
+  logger('git add files');
+  // git add files
+  let index = await remoteRepo.refreshIndex();
+  await Promise.all(files.map(file => index.addByPath(file)));
+  await index.write();
+  let oid = await index.writeTree();
+
+  logger('git commit');
+  // commit
+  let author = git.Signature.default(remoteRepo);
+  let committer = git.Signature.default(remoteRepo);
+  await remoteRepo.createCommit("HEAD", author, committer, "message", oid, []);
+
+  logger(`git clone ${remoteBuild} ${remote} --bare`);
+  await git.Clone(remoteBuild, remote, {bare: 1});
+}
+
+describe('extract compare push workflow', function() {
 
   beforeEach(() => {
+    log = [];
     return fs.remove('./tmp');
   });
 
-  it('push updated services - from v1 to v2', async function() {
-    // setup
-    console.log('setup (remote)');
-    console.log('-----');
-    console.log('git init');
-    let remoteRepo = await git.Repository.init('./tmp/remote', 0);
-    // run js doc
-    console.log('jsdoc ./test/ver1');
-    let v1 = runJsDoc({"include": './test/ver1', "includePattern": ".+\\.(js)?$",}).services;
-    let files = await saveToDir('./tmp/remote', v1);
-
-    console.log('git add files');
-    // git add files
-    let index = await remoteRepo.refreshIndex();
-    await Promise.all(files.map(file => index.addByPath(file)));
-    await index.write();
-    let oid = await index.writeTree();
-
-    console.log('git commit');
-    // commit
-    var author = git.Signature.default(remoteRepo);
-    var committer = git.Signature.default(remoteRepo);
-    await remoteRepo.createCommit("HEAD", author, committer, "message", oid, []);
+  afterEach(function(){
+    if (true /*this.currentTest.state == 'failed'*/) {
+      log.forEach(comment);
+    }
+  });
 
 
-    console.log('');
-    console.log('create bare remote (/bare)');
-    console.log('------------------');
-    console.log('git clone ./tmp/remote ./tmp/bare --bare');
-    await git.Clone('./tmp/remote', './tmp/bare', {bare: 1});
-
-    await extractDocs('./tmp/bare', './tmp/local', {"include": './test/ver2', "includePattern": ".+\\.(js)?$"});
+  it('update remote with changes from v2 over v1', async function() {
+    await createRemoteOnVer1();
+    logger('run test');
+    logger('--------');
+    await extractDocs(remote, './tmp/local', {"include": './test/ver2', "includePattern": ".+\\.(js)?$"}, logger);
 
   });
 
