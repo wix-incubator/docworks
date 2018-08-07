1e5165e03cef08c75e332553e3570f8caf5713a2
yoav@wix.com <yoav@wix.com>
2018-08-07 09:22:02 +0300

    unit test level testing

--- wix-note/src/index.js
+++ wix-note/src/index.js
@@ -12,3 +12,33 @@ exports.defineTags = function(dictionary) {
   });
 
 };
+
+exports.extendDocworksKey = 'note';
+
+function extendDocworks(doclet) {
+  return doclet.notes;
+}
+exports.extendDocworksService = extendDocworks;
+exports.extendDocworksProperty = extendDocworks;
+exports.extendDocworksOperation = extendDocworks;
+exports.extendDocworksMessage = extendDocworks;
+
+function mergeNotes(newNotes, repoNotes) {
+  if (!!newNotes && !!repoNotes && !!newNotes.length && !!repoNotes.length) {
+    if (newNotes.length !== repoNotes.length)
+      return {value: newNotes, changed: true};
+    for (let i=0; i < newNotes.length; i++)
+      if (newNotes[i] !== repoNotes[i])
+        return {value: newNotes, changed: true};
+    return {value: newNotes, changed: false};
+  }
+  else if (!newNotes && !repoNotes)
+    return {value: newNotes, changed: false};
+  else
+    return {value: newNotes, changed: true};
+}
+
+exports.docworksMergeService = mergeNotes;
+exports.docworksMergeProperty = mergeNotes;
+exports.docworksMergeOperation = mergeNotes;
+exports.docworksMergeMessage = mergeNotes;
--- wix-note/test/wix-note.spec.js
+++ wix-note/test/wix-note.spec.js
@@ -3,7 +3,7 @@ import chaiSubset from 'chai-subset';
 const expect = chai.expect;
 chai.use(chaiSubset);
 
-import {defineTags} from '../src/index';
+import {defineTags, extendDocworksService, docworksMergeService} from '../src/index';
 
 describe('wix-note', function() {
 
@@ -15,14 +15,6 @@ describe('wix-note', function() {
       }
     }
   }
-
-  it('should register the snippet tag', function() {
-    let dictionary = makeDict();
-
-    defineTags(dictionary);
-
-    expect(dictionary.tags).to.have.key('note');
-  });
 
   function makeDoclet() {
     return {
@@ -42,8 +34,15 @@ describe('wix-note', function() {
     }
   }
 
+  it('should register the snippet tag', function() {
+    let dictionary = makeDict();
 
-  it.only('should copy note from tag to doclet', function() {
+    defineTags(dictionary);
+
+    expect(dictionary.tags).to.have.key('note');
+  });
+
+  it('should copy note from tag to doclet', function() {
     let dictionary = makeDict();
     defineTags(dictionary);
 
@@ -56,5 +55,42 @@ describe('wix-note', function() {
         ['this is a note']
     });
   });
+
+  it('should extract notes from doclet', function() {
+    let notes = extendDocworksService({notes: ['a', 'b']});
+    expect(notes).to.containSubset(['a', 'b']);
+  });
+
+  describe('merge', function() {
+    it('should report no change if no notes', function() {
+      let mergeResult = docworksMergeService(undefined, undefined);
+      expect(mergeResult.value).to.be.undefined;
+      expect(mergeResult.changed).to.be.false;
+    });
+
+    it('should report no change if notes did not change', function() {
+      let mergeResult = docworksMergeService(['a', 'b'], ['a', 'b']);
+      expect(mergeResult.value).to.containSubset(['a', 'b']);
+      expect(mergeResult.changed).to.be.false;
+    });
+
+    it('should report change if notes added', function() {
+      let mergeResult = docworksMergeService(['a', 'b'], undefined);
+      expect(mergeResult.value).to.containSubset(['a', 'b']);
+      expect(mergeResult.changed).to.be.true;
+    });
+
+    it('should report changed if notes removed', function() {
+      let mergeResult = docworksMergeService(undefined, ['a', 'b']);
+      expect(mergeResult.value).to.be.undefined;
+      expect(mergeResult.changed).to.be.true;
+    });
+
+    it('should report change notes changed', function() {
+      let mergeResult = docworksMergeService(['a', 'c'], ['a', 'b']);
+      expect(mergeResult.value).to.containSubset(['a', 'c']);
+      expect(mergeResult.changed).to.be.true;
+    });
+  })
 
 });
