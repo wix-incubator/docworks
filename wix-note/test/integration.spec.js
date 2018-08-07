887f25efbb04da7c09d0f5c02d730b4f008159cf
yoav@wix.com <yoav@wix.com>
2018-08-07 11:17:20 +0300

    integration tests for wix-notes

--- wix-note/test/integration.spec.js
+++ wix-note/test/integration.spec.js
@@ -1,63 +1,119 @@
 import runJsDoc from 'docworks-jsdoc2spec';
+import {merge} from 'docworks-repo';
 import chai from 'chai';
 import chaiSubset from 'chai-subset';
 
 const expect = chai.expect;
 chai.use(chaiSubset);
 
+const SOURCE_integration_SERVICE = {
+  "include": [
+    "test/integration.service.js"
+  ],
+};
+
+const SOURCE_integration2_SERVICE = {
+  "include": [
+    "test/integration2.service.js"
+  ],
+};
+
 describe('integration test', function() {
-  it('should load the note and add it to the service to the more members', function() {
-    let jsDocRes = runJsDoc({
-      "include": [
-        "test/integration.service.js"
-      ],
-    }, ['src/index']);
+  describe('runJsDoc', function() {
+    it('should load the note and add it to the service to the more members', function() {
+      let jsDocRes = runJsDoc(SOURCE_integration_SERVICE, ['src/index']);
 
-
-
-    expect(jsDocRes).to.containSubset({
-      services: [
-        {name: 'Service', memberOf: 'aNamespace',
-          operations: [
-            { name: 'operation',
-              srcDocs: {
-                summary: 'an operation'
-              },
-              extra: {
-                notes: ['this is a note about the member']
+      expect(jsDocRes).to.containSubset({
+        services: [
+          {name: 'Service', memberOf: 'aNamespace',
+            operations: [
+              { name: 'operation',
+                srcDocs: {
+                  summary: 'an operation'
+                },
+                extra: {
+                  notes: ['this is a note about the member']
+                }
               }
-            }
-          ]
-        }
-      ]
+            ]
+          }
+        ]
+      });
+    });
+
+    it('should load multiple notes add them to the service to the more members', function() {
+      let jsDocRes = runJsDoc(SOURCE_integration_SERVICE, ['src/index']);
+
+      expect(jsDocRes).to.containSubset({
+        services: [
+          {name: 'Service', memberOf: 'aNamespace',
+            operations: [
+              { name: 'operation2',
+                srcDocs: {
+                  summary: 'an operation'
+                },
+                extra: {
+                  notes: ['this is a note about the member', 'this is a second note']
+                }
+              }
+            ]
+          }
+        ]
+      });
     });
   });
 
-  it('should load multiple notes add them to the service to the more members', function() {
-    let jsDocRes = runJsDoc({
-      "include": [
-        "test/integration.service.js"
-      ],
-    }, ['src/index']);
+  describe('merge', function() {
+    it('should detect changed note', function() {
+      let repo = runJsDoc(SOURCE_integration_SERVICE, ['src/index']);
+      let newRepo = runJsDoc(SOURCE_integration2_SERVICE, ['src/index']);
 
+      let mergeResult = merge(newRepo.services, repo.services, ['src/index']);
 
+      expect(mergeResult.messages).to.containSubset(['Service aNamespace.Service operation operation has changed extra.notes']);
 
-    expect(jsDocRes).to.containSubset({
-      services: [
-        {name: 'Service', memberOf: 'aNamespace',
-          operations: [
-            { name: 'operation2',
-              srcDocs: {
-                summary: 'an operation'
-              },
-              extra: {
-                notes: ['this is a note about the member', 'this is a second note']
+      expect(mergeResult).to.containSubset({
+        repo: [
+          {name: 'Service', memberOf: 'aNamespace',
+            operations: [
+              { name: 'operation',
+                srcDocs: {
+                  summary: 'an operation'
+                },
+                extra: {
+                  notes: ['this note has changed']
+                }
               }
-            }
-          ]
-        }
-      ]
+            ]
+          }
+        ]
+      });
     });
-  });
 
+    it('should detect added note', function() {
+      let repo = runJsDoc(SOURCE_integration_SERVICE, ['src/index']);
+      let newRepo = runJsDoc(SOURCE_integration2_SERVICE, ['src/index']);
+
+      let mergeResult = merge(newRepo.services, repo.services, ['src/index']);
+
+      expect(mergeResult.messages).to.containSubset(['Service aNamespace.Service operation operation2 has changed extra.notes']);
+
+      expect(mergeResult).to.containSubset({
+        repo: [
+          {name: 'Service', memberOf: 'aNamespace',
+            operations: [
+              { name: 'operation2',
+                srcDocs: {
+                  summary: 'an operation'
+                },
+                extra: {
+                  notes: ['this is a note about the member', 'this is a second note', 'this is a new note']
+                }
+              }
+            ]
+          }
+        ]
+      });
+    });
+  })
 });
---
+++ wix-note/test/integration2.service.js
@@ -0,0 +1,38 @@
+/**
+ * @class Service
+ * @summary A test service
+ * @memberof aNamespace
+ */
+class Service {
+  /**
+   * @function operation
+   * @memberof aNamespace.Service
+   * @summary an operation
+   * @description
+   * the description of the operation
+   *
+   * @param {string} input the input
+   * @note this note has changed
+   */
+  operation(input) {
+
+  }
+
+  /**
+   * @function operation2
+   * @memberof aNamespace.Service
+   * @summary an operation
+   * @description
+   * the description of the operation
+   *
+   * @param {string} input the input
+   * @note this is a note about the member
+   * @note this is a second note
+   * @note this is a new note
+   */
+  operation2(input) {
+
+  }
+}
+
+export default Service;
