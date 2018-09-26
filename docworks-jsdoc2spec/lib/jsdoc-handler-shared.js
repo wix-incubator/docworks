83c4ddebd6fe8e3923d2ff68aee5b25952be45ea
yoav@wix.com <yoav@wix.com>
2018-09-26 16:58:45 +0300

    added function as well as Function as the name for the function type

--- docworks-jsdoc2spec/lib/jsdoc-handler-shared.js
+++ docworks-jsdoc2spec/lib/jsdoc-handler-shared.js
@@ -14,6 +14,7 @@ const builtInTypes = {
     'Date': 'Date',
     'Array': 'Array',
     'Function': 'Function',
+    'function': 'Function',
     'Promise': 'Promise',
     'Object': 'Object',
     'any': 'any',
