76cb6d9bd745bcc48a5b766f896f59cbe32a33ea
Nir Moav <nirm@wix.com>
2019-06-05 14:04:02 +0300

    changing logger to support multiple arguments (like console)

--- docworks-cli/src/logger.js
+++ docworks-cli/src/logger.js
@@ -1,15 +1,15 @@
 import chalk from 'chalk';
 
 const nvl = (_) => _?_:'';
-export const rawLog = (_) => console.log(_);
-export const details = (_) => console.log(`    ${chalk.gray(_)}`);
+export const rawLog = (...args) => console.log(...args);
+export const details = (...args) => console.log(`    ${chalk.gray(...args)}`);
 export const config = (cmd, args) => console.log(`${chalk.whiteBright(cmd)} ${chalk.white(nvl(args))}` );
 export const command = (cmd, args) => console.log(`${chalk.green('>')} ${chalk.whiteBright(cmd)} ${chalk.white(nvl(args))}` );
 export const newLine = () => console.log();
-export const log = (_) => console.log(`${chalk.white(_)}`);
-export const warn = (_) => console.log(`${chalk.yellow(_)}`);
-export const error = (_) => console.log(`${chalk.red(_)}`);
-export const success = (_) => console.log(`${chalk.green(_)}`);
+export const log = (...args) => console.log(`${chalk.white(...args)}`);
+export const warn = (...args) => console.log(`${chalk.yellow(...args)}`);
+export const error = (...args) => console.log(`${chalk.red(...args)}`);
+export const success = (...args) => console.log(`${chalk.green(...args)}`);
 export const jsDocErrors = (errors) => {
   if (errors.length > 0) {
     errors.forEach(_ => {
