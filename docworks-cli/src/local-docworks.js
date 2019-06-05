91b25dc448cd00ccb89f6bc6c9bd5f3a08d5604d
Nir Moav <nirm@wix.com>
2019-06-05 16:06:17 +0300

    cleanup

--- docworks-cli/src/local-docworks.js
+++ docworks-cli/src/local-docworks.js
@@ -11,10 +11,10 @@ const VEE_CHAR = '\u2713';
 
 function logTaskConfig(outputDirectory, workingDir, projectSubdir, jsDocSources, plugins) {
     logger.newLine();
-    logger.config(`output dir:        `, outputDirectory); // the dist!
-    logger.config(`working dir:       `, workingDir); // the temp dir
-    logger.config(`project dir:       `, projectSubdir); // the project where the sources are (i.e. wix-data-core)
-    logger.config(`jsdoc sources:     `, JSON.stringify(jsDocSources)); // jsDoc
+    logger.config(`output dir:        `, outputDirectory); // the dist for docworks
+    logger.config(`working dir:       `, workingDir); // the (temp) working dir
+    logger.config(`project dir:       `, projectSubdir); // the project name (namespace/subdir for working dir)
+    logger.config(`jsdoc sources:     `, JSON.stringify(jsDocSources)); // jsDocs
     logger.config(`plugins:           `, plugins.join(', '));
     logger.newLine();
 }
@@ -47,9 +47,9 @@ function getAllFilesInDir(path) {
 function getChangedAndNewSrcFiles(srcDir, targetDir) {
     const differentFiles = [];
     const allFilesInSrc = getAllFilesInDir(srcDir);
-    for (let i=0; i < allFilesInSrc.length; i++) {
-        let srcFilePath = allFilesInSrc[i];
-        let targetFilePath = srcFilePath.replace(srcDir, targetDir);
+    for (let i = 0; i < allFilesInSrc.length; i++) {
+        const srcFilePath = allFilesInSrc[i];
+        const targetFilePath = srcFilePath.replace(srcDir, targetDir);
         if (fs.existsSync(targetFilePath)) {
             if (areFilesDifferent(srcFilePath, targetFilePath)) {
                 differentFiles.push(srcFilePath);
@@ -61,13 +61,13 @@ function getChangedAndNewSrcFiles(srcDir, targetDir) {
     return differentFiles;
 }
 
-function areFilesDifferent(src, dest) {
-    return fs.readFileSync(src, 'utf8') !== fs.readFileSync(dest, 'utf8');
-}
+const areFilesDifferent = (fileA, fileB) => readFileContent(fileA) !== readFileContent(fileB);
+const readFileContent = fileToRead => fs.readFileSync(fileToRead, 'utf8');
 
 async function copyDiffedFilesToOutputDirectory(srcDir, targetDir) {
     const differentFiles = getChangedAndNewSrcFiles(srcDir, targetDir);
     if (!differentFiles || differentFiles.length === 0) {
+        logger.log('-- No changes detected.');
         return;
     }
 
@@ -81,31 +81,40 @@ async function copyDiffedFilesToOutputDirectory(srcDir, targetDir) {
 async function cloneGitRepoToDirectory(remoteRepo, targetDir) {
     await fs.ensureDir(targetDir);
 
-    let baseGit = new Git();
+    const baseGit = new Git();
     logger.command('git', `clone ${remoteRepo} ${targetDir}`);
     await baseGit.clone(remoteRepo, targetDir);
 
 }
 
-async function gitCheckoutNewBranch(repoDirectory, branchName, fallbackBranchToCreate) {
-    let localGit = new Git(repoDirectory);
+async function gitCheckoutExistingOrNewBranch(repoDirectory, branchName, fallbackBranchToCreate) {
     if (branchName) {
-        try {
-            logger.command('git', `checkout ${branchName}`);
-            await localGit.checkout(branchName);
-        }
-        catch (e) {
-            await localGit.checkout(`-b${fallbackBranchToCreate}`);
-            logger.warn(`cant checkout branch ${branchName}. created ${fallbackBranchToCreate} on top of master`);
-        }
+        await checkoutExistingBranch(repoDirectory, branchName, fallbackBranchToCreate);
     } else {
-        try {
-            logger.command('git', `checkout -b ${fallbackBranchToCreate}`);
-            await localGit.checkout(`-b${fallbackBranchToCreate}`);
-        } catch (e) {
-            logger.error('Failed to create a new branch on top of master. Aborting.');
-            process.exit(1);
-        }
+        await checkoutNewBranch(repoDirectory, fallbackBranchToCreate);
+    }
+}
+
+async function checkoutExistingBranch(repoDirectory, branchName, fallbackBranchName) {
+    const localGit = new Git(repoDirectory);
+    try {
+        logger.command('git', `checkout ${branchName}`);
+        await localGit.checkout(branchName);
+    }
+    catch (e) {
+        await checkoutNewBranch(repoDirectory, fallbackBranchName);
+        logger.warn(`cant checkout branch ${branchName}. created ${fallbackBranchName} on top of master`);
+    }
+}
+
+async function checkoutNewBranch(repoDirectory, branchNameToCreate) {
+    const localGit = new Git(repoDirectory);
+    try {
+        logger.command('git', `checkout -b ${branchNameToCreate}`);
+        await localGit.checkout(`-b${branchNameToCreate}`);
+    } catch (e) {
+        logger.error('Failed to create a new branch on top of master. Aborting.');
+        process.exit(1);
     }
 }
 
@@ -122,7 +131,7 @@ export default async function localDocworks(remoteRepo, branch, outputDirectory,
         await cloneGitRepoToDirectory(remoteRepo, tmpDir);
 
         const workingSubdir = join(tmpDir, projectDir);
-        await gitCheckoutNewBranch(workingSubdir, branch, createUniqueId('localdocs-'));
+        await gitCheckoutExistingOrNewBranch(workingSubdir, branch, createUniqueId('localdocs-'));
 
         logger.command('docworks', `readServices ${workingSubdir}`);
         const repoContent = await readFromDir(workingSubdir);
@@ -146,7 +155,7 @@ export default async function localDocworks(remoteRepo, branch, outputDirectory,
         logger.success(VEE_CHAR, 'local docworks completed successfully, see output: ' + outputDirectory);
     }
     catch (error) {
-        logger.error('failed to complete workflow\n' + error.stack);
+        logger.error('Failed to complete workflow\n', error.stack);
         throw error;
     }
 
