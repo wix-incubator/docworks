24c68378b6fde733955f8287c70bdac0a12c6d95
Karin Agan <karinag@wix.com>
2019-09-24 13:18:15 +0300

    fix(docworks-dts): fix failing tests

--- docworks-dts/test/$w-fixer.test.js
+++ docworks-dts/test/$w-fixer.test.js
@@ -9,44 +9,44 @@ describe('convert docworks to dts with $w plugin', () => {
       services.push(require(servicePath))
     }
 
-    return docworksToDts(services, {run$wPlugin: true}).pluginDeclaration['$w']
+    return docworksToDts(services, {run$wFixer: true}).fixerDeclaration['$w']
   }
 
   function getMainDeclarationDts() {
     const services = [require('./services/$w.service.json')]
 
-    return docworksToDts(services, {run$wPlugin: true}).mainDeclaration
+    return docworksToDts(services, {run$wFixer: true}).mainDeclaration
   }
 
   describe('main declaration file content', () => {
     test('should not include $w module declaration', () => {
       const dts = getMainDeclarationDts()
-      const unexpectedDeceleration = `declare module '$w' {`
+      const unexpectedDeceleration = 'declare module \'$w\' {'
 
       expect(dts).not.toContain(unexpectedDeceleration)
-    });
+    })
 
     test('should include dataset alias declaration', () => {
       const dts = getMainDeclarationDts()
-      const expectedDeceleration = `type dataset = wix_dataset.Dataset;`
+      const expectedDeceleration = 'type dataset = wix_dataset.Dataset;'
 
       expect(dts).toContain(expectedDeceleration)
-    });
+    })
 
     test('should include router_dataset alias declaration', () => {
       const dts = getMainDeclarationDts()
-      const expectedDeceleration = `type router_dataset = wix_dataset.DynamicDataset;`
+      const expectedDeceleration = 'type router_dataset = wix_dataset.DynamicDataset;'
 
       expect(dts).toContain(expectedDeceleration)
-    });
+    })
 
     test('should include $w service callbacks declaration', () => {
       const dts = getMainDeclarationDts()
-      const expectedDeceleration = `type GoogleMapClickEvent = (event: $w.GoogleMapClickEvent)=>void;`
+      const expectedDeceleration = 'type GoogleMapClickEvent = (event: $w.GoogleMapClickEvent)=>void;'
 
       expect(dts).toContain(expectedDeceleration)
-    });
-  });
+    })
+  })
 
   describe('$w file content', () => {
     describe('triple slash directive', () => {
@@ -55,8 +55,8 @@ describe('convert docworks to dts with $w plugin', () => {
         const expectedDeceleration = '/// <reference path="../common/utilityTypes.d.ts" />'
 
         expect(dts).toContain(expectedDeceleration)
-      });
-    });
+      })
+    })
 
     describe('TypeNameToSdkType', () => {
       test('should generate type declaration', () => {
@@ -64,31 +64,31 @@ describe('convert docworks to dts with $w plugin', () => {
         const expectedDeceleration = 'declare type TypeNameToSdkType = {'
 
         expect(dts).toContain(expectedDeceleration)
-      });
+      })
 
       test('should include queryable services declaration', () => {
         const dts = get$wDts('./services/Gallery.service.json')
         const expectedDeceleration = 'Gallery: $w.Gallery;'
 
         expect(dts).toContain(expectedDeceleration)
-      });
+      })
 
       test('should not include unqueryable services declaration', () => {
         const dts = get$wDts('./services/CartIcon.service.json')
         const unexpectedDeceleration = 'CartIcon: $w.CartIcon;'
 
         expect(dts).not.toContain(unexpectedDeceleration)
-      });
-    });
+      })
+    })
 
     describe('WixElementSelector', () => {
       test('should generate WixElementSelector type', () => {
         const dts = get$wDts()
-        const expectedDeceleration = `type WixElementSelector = PageElementsMap & IntersectionArrayAndBase<TypeNameToSdkType, 'Document'>`
+        const expectedDeceleration = 'type WixElementSelector = PageElementsMap & IntersectionArrayAndBase<TypeNameToSdkType, \'Document\'>'
 
         expect(dts).toContain(expectedDeceleration)
-      });
-    });
+      })
+    })
 
     describe('$w function declaration', () => {
       test('should generate $w function declaration', () => {
@@ -96,8 +96,8 @@ describe('convert docworks to dts with $w plugin', () => {
         const expectedDeceleration = 'declare function $w<T extends keyof WixElementSelector>(selector: T): WixElementSelector[T]'
 
         expect(dts).toContain(expectedDeceleration)
-      });
-    });
+      })
+    })
 
     describe('$w namespace declaration', () => {
       test('should generate $w namespace declaration', () => {
@@ -105,36 +105,36 @@ describe('convert docworks to dts with $w plugin', () => {
         const expectedDeceleration = 'declare namespace $w {'
 
         expect(dts).toContain(expectedDeceleration)
-      });
+      })
 
       test('should generate at function declaration', () => {
         const dts = get$wDts()
         const expectedDeceleration = 'function at(context: $w.Event.EventContext): $w.$w;'
 
         expect(dts).toContain(expectedDeceleration)
-      });
+      })
 
       test('should generate onReady function declaration', () => {
         const dts = get$wDts()
         const expectedDeceleration = 'function onReady(initFunction: $w.ReadyHandler): void;'
 
         expect(dts).toContain(expectedDeceleration)
-      });
+      })
 
       test('should generate $w function type declaration', () => {
         const dts = get$wDts()
         const expectedDeceleration = 'type $w = <T extends keyof WixElementSelector>(selector: T)=>WixElementSelector[T]'
 
         expect(dts).toContain(expectedDeceleration)
-      });
-    });
-  });
+      })
+    })
+  })
 
   describe('a repo', () => {
     test('should delete $w module and add dataset aliases', async () => {
       let repo = await readFromDir('./test/services')
 
-      let dts = docworksToDts(repo.services, {run$wPlugin: true})
+      let dts = docworksToDts(repo.services, {run$wFixer: true})
 
       expect(dts.mainDeclaration).toMatchSnapshot()
     })
@@ -142,9 +142,9 @@ describe('convert docworks to dts with $w plugin', () => {
     test('should generate $w definition content', async () => {
       let repo = await readFromDir('./test/services')
 
-      let dts = docworksToDts(repo.services, {run$wPlugin: true})
+      let dts = docworksToDts(repo.services, {run$wFixer: true})
 
-      expect(dts.pluginDeclaration['$w']).toMatchSnapshot()
+      expect(dts.fixerDeclaration['$w']).toMatchSnapshot()
     })
-  });
+  })
 })
