52509d909411e9009e4db2721d3de2d1ff79a84b
Dor Itzhaki <dori@wix.com>
2019-11-07 14:54:40 +0200

    resolve plugins that are not direct dependencies of the processed module

--- docworks-cli/src/plugins.js
+++ docworks-cli/src/plugins.js
@@ -2,22 +2,33 @@ const resolve = require('resolve')
 const logger = require('./logger')
 
 function resolveAndInitPlugins(plugins) {
-  return (plugins ? (Array.isArray(plugins) ? plugins : [plugins]) : [])
-    .map(pluginCmd => {
-        let [plugin, param] = pluginCmd.split(/:(.+)/)
-        plugin = resolve.sync(plugin, {basedir: '.'})
-        try {
-          const pluginModule = require(plugin)
-          if (param && pluginModule.init) {
-            pluginModule.init(param)
-          }
+  return (plugins ? (Array.isArray(plugins) ? plugins : [plugins]) : []).map(
+    pluginCmd => {
+      let [pluginName, param] = pluginCmd.split(/:(.+)/)
+      try {
+        const pluginPath = resolvePluginPath(pluginName)
+        const pluginModule = require(pluginPath)
+        if (param && pluginModule.init) {
+          pluginModule.init(param)
         }
-        catch (err) {
-            logger.error(`could not require plugin ${plugin}`)
-        }
-        return plugin
+        return pluginPath
+      } catch (err) {
+        logger.error(`could not require plugin ${pluginName}`, err)
+        throw err
       }
-    )
+    }
+  )
+}
+
+const resolvePluginPath = pluginName => {
+  try {
+    // resolve from the working directory
+    return resolve.sync(pluginName, { basedir: '.' })
+  } catch (e) {
+    // didn't find the plugin in the working directory
+  }
+  // resolve from the running docworks module directory
+  return resolve.sync(pluginName, { basedir: __dirname })
 }
 
 async function runPlugins(plugins, pluginFunction, ...params) {
