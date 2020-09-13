c1ad12ae277ecb3e7259212ae3d7c3e67975325d
roib <roib@wix.com>
2020-09-13 09:20:17 +0300

    wix-jsdoc-babel cleanup

--- wix-jsdoc-babel/src/index.js
+++ wix-jsdoc-babel/src/index.js
@@ -1,6 +1,5 @@
 const { includes, last, omit } = require('lodash')
 const { transform } = require('@babel/core')
-const jsdocRegex = require('jsdoc-regex')
 const defaultTransformConfigPath = require.resolve('./transform.config.js')
 
 // jsdoc with requizzle loads the modules twice - so the only way to move config between the two runs is using global
@@ -11,41 +10,13 @@ function shouldProcessFile(filename, { extensions }) {
   return includes(extensions, last(filename.split('.')))
 }
 
-function countCharInRange(source, from, to, char) {
-  let cnt = 0
-  for (let i = from; i < to; i += 1) {
-    if (source[i] === char) {
-      cnt += 1
-    }
-  }
-  return cnt
-}
-
-function stripWhitespace(text) {
-  return text.replace(/ /g, '')
-}
-
-function processFile(source, options, doclets) {
-  const regex = jsdocRegex()
-  let lastIndex = 0
-  let linesCount = 1
-  let match = regex.exec(source)
-  while (match !== null) {
-    const { index } = match
-    linesCount += countCharInRange(source, lastIndex, index, '\n')
-    const key = stripWhitespace(match[0])
-    // eslint-disable-next-line no-param-reassign
-    doclets[key] = linesCount
-    lastIndex = index
-    match = regex.exec(source)
-  }
+function processFile(source, options) {
   return transform(source, omit(options, 'extensions')).code
 }
 
 let doclets = {}
 const handlers = {
   beforeParse: (event) => {
-    doclets = {}
     const options = {
       extensions: global.wixJsdocBabelExtensions,
       filename: event.filename,
